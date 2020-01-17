7f42fe14a4f873b82264c12b3850c0677ee288f9
Claudia <claudia.alphonse@coditas.com>
2020-01-17 16:42:25 +0530

    added import changes

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -8,11 +8,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoctorsService } from '../doctors/doctors.service';
 import { Doctors } from '../doctors/doctors.entity';
 import { BetterDoctorConfig } from 'src/config/better-doctor-config';
+import { Credentials } from 'src/credentials/credentials.entity';
 
 @Module({
-    imports: [HttpModule, TypeOrmModule.forFeature([Appointments, Doctors])],
-      controllers: [AppointmentsController],
-      providers: [AppointmentsService, DoctorsService, BetterDoctorService, BetterDoctorConfig],
-      exports: [AppointmentsService],
+  imports: [HttpModule, TypeOrmModule.forFeature([Appointments, Doctors, Credentials])],
+  controllers: [AppointmentsController],
+  providers: [AppointmentsService, DoctorsService, BetterDoctorService, BetterDoctorConfig],
+  exports: [AppointmentsService],
 })
-export class AppointmentsModule {}
+export class AppointmentsModule { }
